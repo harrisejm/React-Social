c6ad302bb5c5a29368b410baf616158a471cb97f
Brian Palowski and Eddie Harris <brian@>
2018-09-04 14:46:50 -0700

    add initial file template for social media clone

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function App(){
+
+var boxes = {
+  border: 'solid',
+  color: 'green'
+}
+
+  return (
+    <div>
+      <table>
+        <tr>
+          <td style={boxes}>
+            fvfg
+          </td>
+          <td style={boxes}>
+           fbfdgdg
+         </td>
+          <td style={boxes}>
+          dgdgg
+        </td>
+</tr>
+
+      </table>
+
+      <h1>Hello</h1>
+    </div>
+
+  );
+}
+
+export default App;
